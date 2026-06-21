62ffd16bc40c0b719afa3ec53c3d9fa30f026666
QingYingX <2621529331@qq.com>
2026-06-21 20:45:07 +0800

    feat: 移除沙盒管理功能

--- apps/web-antd/src/api/guoba/index.ts
+++ apps/web-antd/src/api/guoba/index.ts
@@ -3,6 +3,5 @@ export * from './js-plugin';
 export * from './miao';
 export * from './plugin';
 export * from './preferences';
-export * from './sandbox';
 export * from './system';
 export * from './types';
